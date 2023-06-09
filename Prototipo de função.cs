e57cba2ec618c6122510e91c174155201bb7b62a
LeonardoMatioliGomes <127799008+LeonardoMatioliGomes@users.noreply.github.com>
2023-06-09 11:34:01 -0300

    Prototipo de função.cs

--- Prototipo de função.cs
+++ Prototipo de função.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program
 {
+    //código define as funções (métodos) que realizam as operações matemáticas. Cada método recebe dois números ("a" e "b") e retorna o resultado da operação correspondente. A função "Dividir" também inclui uma verificação para evitar a divisão por zero, exibindo uma mensagem de erro caso o divisor seja zero.
     static double Somar(double a, double b)
     {
         return a + b;
@@ -29,21 +30,26 @@ class Program
             return a / b;
         }
     }
-
+    
+    //método "Main", que é o ponto de entrada do programa. Ele define duas variáveis "x" e "y" com os valores 4 e 8
     static void Main(string[] args)
     {
         double x = 4;
         double y = 8;
-
+        
+        //feita a soma de "x" e "y"
         double resultado = Somar(x, y);
         Console.WriteLine(x + " + " + y + " = " + resultado);
 
+        //feita a subtração de "x" e "y"
         resultado = Subtrair(x, y);
         Console.WriteLine(x + " - " + y + " = " + resultado);
 
+        //feita a multiplicação de "x" e "y"
         resultado = Multiplicar(x, y);
         Console.WriteLine(x + " * " + y + " = " + resultado);
 
+        //feita a divisão de "x" e "y"
         resultado = Dividir(x, y);
         Console.WriteLine(x + " / " + y + " = " + resultado);
 
